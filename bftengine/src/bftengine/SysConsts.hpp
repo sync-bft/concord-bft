079cec714005df83cd33aa850035db58941e1b40
Kerry Lu <kerry.lu@duke.edu>
2020-06-30 12:11:53 -0700

    Changing kWorkWindowSize and checkpointWindowSize

--- bftengine/src/bftengine/SysConsts.hpp
+++ bftengine/src/bftengine/SysConsts.hpp
@@ -43,9 +43,9 @@ constexpr int MaxNumberOfReplicas = 64;
 // Work windows and intervals
 ///////////////////////////////////////////////////////////////////////////////
 
-constexpr uint16_t kWorkWindowSize = 300;
+constexpr uint16_t kWorkWindowSize = 2;
 
-constexpr uint16_t checkpointWindowSize = 150;
+constexpr uint16_t checkpointWindowSize = 1;
 static_assert(kWorkWindowSize == 2 * checkpointWindowSize, "kWorkWindowSize != 2 * checkpointWindowSize");
 
 // TODO(GG): check the value in config:
