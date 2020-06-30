a40354e460bdd24030261ad8670256e25dacc8ba
Kerry Lu <kerry.lu@duke.edu>
2020-06-30 12:15:04 -0700

    Changing kWorkWindowSize, checkpointWindowSize, maxLegalConcurrentAgreementsByPrimary, and MaxConcurrentFastPaths

--- bftengine/src/bftengine/SysConsts.hpp
+++ bftengine/src/bftengine/SysConsts.hpp
@@ -43,17 +43,17 @@ constexpr int MaxNumberOfReplicas = 64;
 // Work windows and intervals
 ///////////////////////////////////////////////////////////////////////////////
 
-constexpr uint16_t kWorkWindowSize = 2;
+constexpr uint16_t kWorkWindowSize = 6;
 
-constexpr uint16_t checkpointWindowSize = 1;
+constexpr uint16_t checkpointWindowSize = 3;
 static_assert(kWorkWindowSize == 2 * checkpointWindowSize, "kWorkWindowSize != 2 * checkpointWindowSize");
 
 // TODO(GG): check the value in config:
 // (maxConcurrentAgreementsByPrimary should be <= maxLegalConcurrentAgreementsByPrimary)
-constexpr uint16_t maxLegalConcurrentAgreementsByPrimary = 16;
+constexpr uint16_t maxLegalConcurrentAgreementsByPrimary = 1;
 
 // Maximum number of fast paths that are simultaneously in progress.
-constexpr uint16_t MaxConcurrentFastPaths = 75;
+constexpr uint16_t MaxConcurrentFastPaths = 2;
 static_assert(kWorkWindowSize > MaxConcurrentFastPaths + checkpointWindowSize,
               "Violation of kWorkWindowSize > MaxConcurrentFastPaths + checkpointWindowSize");
 static_assert(maxLegalConcurrentAgreementsByPrimary < MaxConcurrentFastPaths,
