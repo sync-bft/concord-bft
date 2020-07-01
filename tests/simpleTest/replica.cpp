a80cfe7e8aaba20f44db08dd7556432c26f1494d
QIAOYI FANG <QF30@duke.edu>
2020-07-01 12:30:30 -0400

    changed the log level in simple test

--- tests/simpleTest/replica.cpp
+++ tests/simpleTest/replica.cpp
@@ -174,6 +174,9 @@ void signalHandler(int signum) {
 }
 
 int main(int argc, char **argv) {
+  // set the log level to debug
+  replicaLogger.setLogLevel(logging::debug)
+  
   ReplicaParams rp;
   parse_params(argc, argv, rp);
 
