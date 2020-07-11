e4e10f95787436ba5d323911e1c73a39d9ec0d68
George Wang <wzy@Georges-MacBook-Pro.local>
2020-07-11 15:47:15 -0400

    adding VoteMsg class

---
+++ bftengine/src/bftengine/messages/VoteMsg.cpp
@@ -0,0 +1,4 @@
+//
+// Created by George Wang on 7/11/20.
+//
+
---
+++ bftengine/src/bftengine/messages/VoteMsg.hpp
@@ -0,0 +1,8 @@
+//
+// Created by George Wang on 7/9/20.
+//
+
+#ifndef CONCORD_BFT_VOTEMSG_HPP
+#define CONCORD_BFT_VOTEMSG_HPP
+
+#endif //CONCORD_BFT_VOTEMSG_HPP
