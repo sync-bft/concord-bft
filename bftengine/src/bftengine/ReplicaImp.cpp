b8d73c91c4e63af634b5fcfc0803ea6912c50083
QIAOYI FANG <QF30@duke.edu>
2020-09-04 12:57:18 -0400

    fixed commit timer for loop

--- bftengine/src/bftengine/ReplicaImp.cpp
+++ bftengine/src/bftengine/ReplicaImp.cpp
@@ -3727,7 +3727,7 @@ void ReplicaImp::onMessage<ProposalMsg>(ProposalMsg *msg) {//Receiving proposalM
 
 void ReplicaImp::onStartCommitTimer(Timers::Handle timer, SeqNum msgSeqNum) {
   LOG_INFO(CNSUS, "Start commit Timer");
-  for (SeqNum msgIterator = lastExecutedSeqNum + 1; msgIterator <= msgSeqNum; msgIterator++;){
+  for (SeqNum msgIterator = lastExecutedSeqNum + 1; msgIterator <= msgSeqNum; msgIterator++){
     SeqNumInfo &seqNumInfo = mainLog->get(msgIterator);
     ProposalMsg *proposal = seqNumInfo.getProposalMsg();
     auto span = concordUtils::startSpan("bft_execute_requests_in_proposal");
