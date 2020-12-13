fc3cb72cfeaf1c00a39b5e4210ab1962f5cf391c
georgezywang <georgezywang@outlook.com>
2020-12-13 13:40:46 -0500

    command line interface for config generation and running bash script

--- build/tests/simpleTest/fabfile.py
+++ build/tests/simpleTest/fabfile.py
@@ -1,18 +1,27 @@
 import fabric
 from fabric import Connection
 from getpass import getpass
+import argparse
 
-hostIP = input("IP address of host: ")
-hostUsername = input("Username of host: ")
-hostPassword = getpass()
+def connect(hostIP, hostUsername):
+    hostPassword = getpass()
+    connection = Connection(host = str(hostIP), port = 22, user = str(hostUsername), connect_kwargs = {'password': str(hostPassword)})
+    return connection
 
-connection = Connection(host = str(hostIP), port = 22, user = str(hostUsername), connect_kwargs = {'password': str(hostPassword)})
+def executeCommand(username, ip, numReplicas, numClients, connection):
+    command = "ssh " + str(username) + "@" + str(ip) + " 'bash -s' < concord-bft/build/tests/simpleTest/runMultipleClients.sh " + str(numReplicas) + " " + str(numClients)
+    connection.run(command, warn=True)
 
-username = input("Username of remote: ")
-ip = input("IP address of remote: ")
-numReplicas = input("Enter number of Replicas: ")
-numClients = input("Enter number of Clients: ")
-
-command = "ssh " + str(username) + "@" + str(ip) + " 'bash -s' < concord-bft/build/tests/simpleTest/runMultipleClients.sh " + str(numReplicas) + " " + str(numClients)
 # example command: "ssh umm420_gmail_com@35.196.156.226 'bash -s' < concord-bft/build/tests/simpleTest/runMultipleClients.sh 3 3"
-connection.run(command, warn=True)
+
+if __name__ == "__main__":
+     parser = argparse.ArgumentParser()
+     parser.add_argument("-ih", "--hostip", help = "ip address of host")
+     parser.add_argument("-uh", "--hostusn", help = "username of host")
+     parser.add_argument("-ir", "--remoteip", help = "ip address of remote")
+     parser.add_argument("-ur", "--remoteusn", help = "username of remote")
+     parser.add_argument("-r", "--replicas", help = "number of replicas", type = int)
+     parser.add_argument("-c", "--clients", help = "number of clients", type = int)
+     args = parser.parse_args()
+     connection = connect(args.hostIP, args.hostUsername)
+     executeCommand(args.remoteusn, args.remoteip, args.replicas, args.clients, connection)
