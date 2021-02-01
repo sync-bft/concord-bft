f4ecf01bee5e3d0135629917d7f467aa34a208f1
Kerry Lu <kerry.lu@duke.edu>
2021-01-31 18:34:31 -0800

    Add starting replica number and starting client number parameters to runMultipleClients.sh and fabfile.py

--- build/tests/simpleTest/fabfile.py
+++ build/tests/simpleTest/fabfile.py
@@ -8,10 +8,10 @@ def connect(ip, username):
     connection = Connection(host = str(ip), port = 22, user = str(username), connect_kwargs = {'password': str(password)})
     return connection
 
-def executeCommand(numReplicas, numClients, connection):
+def executeCommand(numReplicas, numClients, startingReplica, startingClient, connection):
     #connection.run('pkill server')
     #connection.run('pkill client')
-    command = "./runMultipleClients.sh " + str(numReplicas) + " " + str(numClients)
+    command = "./runMultipleClients.sh " + str(numReplicas) + " " + str(numClients) + " " + str(startingReplica) + " " + str(startingClient)
     connection.run(command, warn=True)
 
 # example command: "./runMultipleClients.sh 3 3"
@@ -22,10 +22,12 @@ if __name__ == "__main__":
      parser.add_argument("-ur", "--remoteusn", help = "username of remote")
      parser.add_argument("-r", "--replicas", help = "number of replicas", type = int)
      parser.add_argument("-c", "--clients", help = "number of clients", type = int)
+     parser.add_argument("-sr", "--startingReplica", help = "starting replica number", type = int)
+     parser.add_argument("-sc", "--startingClient", help = "starting client number", type = int)
      args = parser.parse_args()
      connection = connect(args.remoteip, args.remoteusn)
      with connection.cd('~/concord-bft/build/tests/simpleTest'):
         #connection.run('pwd')
-        executeCommand(args.replicas, args.clients, connection)
+        executeCommand(args.replicas, args.clients, args.startingReplica, args.startingClient, connection)
 
-# to call the the script, run python3 fabfile.py -ir *remoteip* -ur *remoteusn* -r *replicas* -c *clients* in terminal and type the remote's password when prompted.
+# to call the the script, run python3 fabfile.py -ir *remoteip* -ur *remoteusn* -r *replicas* -c *clients* -sr *startingReplica* -sc *startingClient* in terminal and type the remote's password when prompted.
--- build/tests/simpleTest/runMultipleClients.sh
+++ build/tests/simpleTest/runMultipleClients.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# arg 1 is number of replicas and arg 2 is number of clients
-# e.g.: ./runMultipleClients.sh 3 3
-# will run 3 replicas and 3 clients
+# arg 1 is number of replicas and arg 2 is number of clients and arg3 is starting replica and arg4 is starting client
+# e.g.: ./runMultipleClients.sh 3 3 0 3
+# will run 3 replicas and 3 clients starting from replica id 0 and client id 3
 
 set -e
 
-counter=0
+counter=$3
 replicaCounter=1
 
 while [ $replicaCounter -le $1 ]
 do
-    ~/concord-bft/build/tests/simpleTest/server -id $counter &#-cf ~/concord-bft/build/tests/simpleTest/scripts/remote_config.txt &
+    ~/concord-bft/build/tests/simpleTest/server -id $counter -cf ~/concord-bft/build/tests/simpleTest/scripts/remote_config.txt &
     processID=$!
     let counter=counter+1
     let replicaCounter=replicaCounter+1
 done
 
+secondCounter=$4
 clientCounter=1
 
 while [ $clientCounter -le $2 ]    
 do
-    ~/concord-bft/build/tests/simpleTest/client -id $counter &#-cf ~/concord-bft/build/tests/simpleTest/scripts/remote_config.txt &
+    ~/concord-bft/build/tests/simpleTest/client -id $secondCounter -cf ~/concord-bft/build/tests/simpleTest/scripts/remote_config.txt &
     processID=$!
     let counter=counter+1
     let clientCounter=clientCounter+1
